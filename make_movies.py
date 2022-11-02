5891b49df4a6a4e4d445d70996934c035dd62018
tjones8620 <thomasjones2700@gmail.com>
2022-11-02 13:50:52 +0000

    Changes to make movies file

--- make_movies.py
+++ make_movies.py
@@ -1,10 +1,22 @@
 import os
 import moviepy.video.io.ImageSequenceClip
-image_folder='/mnt/local/thomas/wr140-cool-covertex/SimulationPlots_Thomas/WR140_hydro_cool_d3l6n064/Density/XY'
-fps=10
+import re
+import numpy as np
 
-image_files = [os.path.join(image_folder,img)
-               for img in sorted(os.listdir(image_folder))
-               if img.endswith(".png")]
-clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
-clip.write_videofile('my_video.mp4')
+def make_movies(image_folder, movie_folder, movie_name):
+    image_folder
+    fps=10
+
+    dir_list = os.listdir(image_folder)
+
+    dir_list.sort(key=lambda test_string : list(
+        map(int, re.findall(r'\d+', test_string))))
+
+    # print(dir_list)
+
+    image_files = [os.path.join(image_folder,img)
+                for img in dir_list
+                if img.endswith(".png")]
+
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+    clip.write_videofile(os.path.join(movie_folder, movie_name), bitrate="12000k")
