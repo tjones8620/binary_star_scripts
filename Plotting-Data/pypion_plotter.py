bcfd41bf9e59a1c7cacb679e9c274846e9acc6fb
tjones8620 <thomasjones2700@gmail.com>
2022-11-02 13:42:34 +0000

    Changes to pypion plotter

--- Plotting-Data/pypion_plotter.py
+++ Plotting-Data/pypion_plotter.py
@@ -26,7 +26,7 @@ class Plot_Functions:
             exit()
             
         ########### Base name of SILO files ############################
-        self.filename=sorted(os.listdir(self.data_path))[0].replace('_level00_0000.00000000.silo','')
+        self.filename=sorted(os.listdir(self.data_path))[4].replace('_level00_0000.00000000.silo','')
         print(self.filename)
 
         ########### Define desired fluid quantity ######################
@@ -42,11 +42,11 @@ class Plot_Functions:
             print(f"Chosen surface: {self.Surface}")
         else:
             print(f"Invalid Surface Choice: {self.Surface}")
+            exit()
 
         ########### Make snapshots of the simulation ###################
         self.evolution = self.make_snapshots(self.data_path, self.filename)
 
-        print(self.evolution.shape)
         ########### Find the dimensions of the simulation ##############
         self.find_dimensions()
         
@@ -55,6 +55,8 @@ class Plot_Functions:
 
         ######### Defining default fps for mp4 ##########
         self.fps=10
+
+    
 
     @staticmethod
     def make_snapshots(data_path, filename):
@@ -105,7 +107,6 @@ class Plot_Functions:
 
     @staticmethod
     def create_image_dir(img_dir, *args):
-        # ImageDir = os.path.join(img_dir, f'SimulationPlots/{self.filename}/{self.Quantity}/{self.Surface}')
         ImageDir = os.path.join(img_dir, f'SimulationPlots/{args[0]}/{args[1]}/{args[2]}')
         try:
             # Create target Directory
@@ -133,85 +134,85 @@ class Plot_Functions:
 
 
 
-    # #######################################################################
-    # # 3D-Surface-Plotter
-    # def ThreeDSurfacePlotter(self, colormap='viridis', movie=False):
+    #######################################################################
+    # 3D-Surface-Plotter
+    def ThreeDSurfacePlotter(self, colormap='viridis', movie=False):
 
-    #     print('3D-Surface-Plotter: Plotting', self.Surface, 'plane')
+        print('3D-Surface-Plotter: Plotting', self.Surface, 'plane')
 
-    #     if (self.Surface == 'XY'): xlabel = 'x'; x = 0; ylabel = 'y'; y = 1 ; z = 2 
-    #     if (self.Surface == 'XZ'): xlabel = 'x'; x = 0; ylabel = 'z'; y = 2 ; z = 1
-    #     if (self.Surface == 'YZ'): xlabel = 'y'; x = 1; ylabel = 'z'; y = 2 ; z = 1
+        if (self.Surface == 'XY'): xlabel = 'x'; x = 0; ylabel = 'y'; y = 1 ; z = 2 
+        if (self.Surface == 'XZ'): xlabel = 'x'; x = 0; ylabel = 'z'; y = 2 ; z = 1
+        if (self.Surface == 'YZ'): xlabel = 'y'; x = 1; ylabel = 'z'; y = 2 ; z = 1
 
-    #     for k in range(len(self.evolution)):
-    #         data = ReadData(self.evolution[k])
-    #         N_level = data.nlevels()
-    #         N_grids = data.ngrid()
-    #         baseline_data = data.get_3Darray(self.Quantity)
-    #         fluid_parameter = baseline_data['data']
-    #         dims_min = (baseline_data['min_extents'] * units.cm).to(units.astrophys.au)
-    #         dims_max = (baseline_data['max_extents'] * units.cm).to(units.astrophys.au)
-    #         time = (baseline_data['sim_time'].value * units.s).to(units.yr)
+        for k in range(len(self.evolution)):
+            data = ReadData(self.evolution[k])
+            N_level = data.nlevels()
+            N_grids = data.ngrid()
+            baseline_data = data.get_3Darray(self.Quantity)
+            fluid_parameter = baseline_data['data']
+            dims_min = (baseline_data['min_extents'] * units.cm).to(units.astrophys.au)
+            dims_max = (baseline_data['max_extents'] * units.cm).to(units.astrophys.au)
+            time = (baseline_data['sim_time'].value * units.s).to(units.yr)
 
-    #         fig, ax = plt.subplots()
-    #         ax.set_xlim(dims_min[0][x].value, dims_max[0][x].value)
-    #         ax.set_ylim(dims_min[0][y].value, dims_max[0][y].value)
-    #         ax.set_xlabel(xlabel + str(dims_min.unit), fontsize=8)
-    #         ax.set_ylabel(ylabel + str(dims_min.unit), fontsize=8)
+            fig, ax = plt.subplots()
+            ax.set_xlim(dims_min[0][x].value, dims_max[0][x].value)
+            ax.set_ylim(dims_min[0][y].value, dims_max[0][y].value)
+            ax.set_xlabel(xlabel + str(dims_min.unit), fontsize=8)
+            ax.set_ylabel(ylabel + str(dims_min.unit), fontsize=8)
 
-    #         for l in range(N_level):  # plotting each levels
+            for l in range(N_level):  # plotting each levels
 
-    #             plot_data = np.array(fluid_parameter)
+                plot_data = np.array(fluid_parameter)
                 
-    #             # Slicer ###############################################################
-    #             slice_location = int(0.5 * N_grids[z])
-    #             if (self.Surface == 'YZ'): sliced_data = plot_data[l][:, :, slice_location - 1]
-    #             if (self.Surface == 'XZ'): sliced_data = plot_data[l][:, slice_location - 1, :]
-    #             if (self.Surface == 'XY'): sliced_data = plot_data[l][slice_location - 1, :, :]
-    #             # Slicer ends #*********************************************************
+                # Slicer ###############################################################
+                slice_location = int(0.5 * N_grids[z])
+                if (self.Surface == 'YZ'): sliced_data = plot_data[l][:, :, slice_location - 1]
+                if (self.Surface == 'XZ'): sliced_data = plot_data[l][:, slice_location - 1, :]
+                if (self.Surface == 'XY'): sliced_data = plot_data[l][slice_location - 1, :, :]
+                # Slicer ends #*********************************************************
 
             
-    #             extents = [dims_min[l][x].value, dims_max[l][x].value, dims_min[l][y].value, dims_max[l][y].value]
+                extents = [dims_min[l][x].value, dims_max[l][x].value, dims_min[l][y].value, dims_max[l][y].value]
 
-    #             # if (self.Surface == 'YZ'): extents = [dims_min[l][y].value, dims_max[l][y].value, dims_min[l][x].value, dims_max[l][x].value]
+                # if (self.Surface == 'YZ'): extents = [dims_min[l][y].value, dims_max[l][y].value, dims_min[l][x].value, dims_max[l][x].value]
 
-    #             image = ax.imshow(np.log10(sliced_data), interpolation="nearest", cmap=colormap,
-    #                               extent=extents,
-    #                               origin="lower",
-    #                               vmin=self.Tolerance[0], vmax=self.Tolerance[1]
-    #                               )
+                image = ax.imshow(np.log10(sliced_data), interpolation="nearest", cmap=colormap,
+                                  extent=extents,
+                                  origin="lower",
+                                  vmin=self.Tolerance[0], vmax=self.Tolerance[1]
+                                  )
                 
                 
-    #             plt.savefig(f"{self.ImageDir}/image{str(k).zfill(3)}.png", bbox_inches="tight", dpi=200)
+                plt.savefig(f"{self.ImageDir}/image{str(k).zfill(3)}.png", bbox_inches="tight", dpi=500)
                 
-    #             if (l == 0):
-    #                 cbaxes = fig.add_axes([0.22, 0.95, 0.575, 0.02])
-    #                 cb = fig.colorbar(image, ax=ax, orientation="horizontal", cax=cbaxes, pad=0.0)
-    #                 cb.ax.tick_params(labelsize=8)
-    #                 tick_locator = ticker.MaxNLocator(nbins=5)
-    #                 cb.locator = tick_locator
-    #                 cb.update_ticks()
+                if (l == 0):
+                    cbaxes = fig.add_axes([0.22, 0.95, 0.575, 0.02])
+                    cb = fig.colorbar(image, ax=ax, orientation="horizontal", cax=cbaxes, pad=0.0)
+                    cb.ax.tick_params(labelsize=8)
+                    tick_locator = ticker.MaxNLocator(nbins=5)
+                    cb.locator = tick_locator
+                    cb.update_ticks()
 
                 
-    #                 # tm = str(f"{time.value:.4f}")
-    #                 # st = "\nTime = " + tm + str(time.unit) + "\n$\log_{10} \left[" + fluid_quantity + "\, (\mathrm{g\,cm}^{-3}) \\right]$"
-    #                 # ax.text(0.3, 0.9, st, color="white", fontsize=8, transform=ax.transAxes)
+                    # tm = str(f"{time.value:.4f}")
+                    # st = "\nTime = " + tm + str(time.unit) + "\n$\log_{10} \left[" + fluid_quantity + "\, (\mathrm{g\,cm}^{-3}) \\right]$"
+                    # ax.text(0.3, 0.9, st, color="white", fontsize=8, transform=ax.transAxes)
 
-    #         plt.close(fig)
-    #         print(f'Time: {time:.2e}.',
-    #               f'Saving snap-{str(k)} to image{str(k).zfill(3)}.png ...')
+            plt.close(fig)
+            print(f'Time: {time:.2e}.',
+                  f'Saving snap-{str(k)} to image{str(k).zfill(3)}.png ...')
 
     
-    #     if movie==True:
+        if movie==True:
 
-    #         image_folder=self.ImageDir
-    #         image_files = [os.path.join(image_folder,img)
-    #                     for img in sorted(os.listdir(image_folder))
-    #                     if img.endswith(".png")]
-    #         clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=self.fps)
-    #         clip.write_videofile(f'{self.ImageDir}/{self.filename}_{self.Quantity}_{self.Surface}.mp4')
+            image_folder=self.ImageDir
+            image_files = [os.path.join(image_folder,img)
+                        for img in sorted(os.listdir(image_folder))
+                        if img.endswith(".png")]
+            clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=self.fps)
+            clip.write_videofile(f'{self.ImageDir}/{self.filename}_{self.Quantity}_{self.Surface}.mp4')
         
-    #     else:
-    #         pass
+        else:
+            pass
         
 
