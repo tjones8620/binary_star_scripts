a95776956dc58216afe8dd9efbdfd228c3d38a3a
tjones8620 <thomasjones2700@gmail.com>
2022-11-17 12:36:34 +0000

    minor changes to argparse_pypionplotter

--- pypion-plotting/argparse_pypionplotter.py
+++ pypion-plotting/argparse_pypionplotter.py
@@ -8,9 +8,9 @@ class ArgparseInputs:
         parser.add_argument("img_dir", help="Path to image directory")
         parser.add_argument("-q", "--fluidquantity", help="Desired fluid quantity to plot", default="Density")
         parser.add_argument("-s", "--surface", help ="3D Plot surface - XY, XZ, YZ", default="XY")
-        parser.add_argument('--tol', nargs='+', type=float, default=[np.log10(1.0e-18), np.log10(2.0e-13)])
+        parser.add_argument('--tol', nargs='+', type=float, default=[np.log10(1.0e-18), np.log10(1.0e-14)])
         parser.add_argument('--cmap', default='viridis')
-        parser.add_argument("-mm", "--make_movie", type=bool, choices=[True, False], default=False)
+        parser.add_argument("-mm", "--make_movie", help="Make movie from images", action="store_true")
         args = parser.parse_args()
 
         self.path = args.Path
