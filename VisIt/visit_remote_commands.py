ca8e71aea014dac5e6f64f57e440cf850ce03f19
tjones8620 <thomasjones2700@gmail.com>
2022-12-16 22:47:28 +0000

    Made changes to visit_remote_commands.py

--- VisIt/visit_remote_commands.py
+++ VisIt/visit_remote_commands.py
@@ -6,19 +6,20 @@ hostname = "mimir.ap.dias.ie"
 
 # Input parameters
 # file_path = "/mnt/massive-stars/data/thomas_simulations/wr140-sims/covertex_start/red_z_res/wr140-hydro-l7n128/"
-file_path = "/mnt/massive-stars/data/thomas_simulations/wr140-sims/late_start/wr140-hydro-n256/"
-file_basename = "wr140_hydro_cool_d3l7n256"
-quantity = "Temperature"
+#file_path = "/mnt/massive-stars/data/thomas_simulations/wr140-sims/late_start/wr140-hydro-n256/"
+#file_basename = "wr140_hydro_cool_d3l7n256"
 
-# file_path = cmdargs.path
-# file_basename = cmdargs.basename
-# quantity = cmdargs.quantity
+#file_path = "/mnt/massive-stars/data/thomas_simulations/wr140-sims/accel_wind/wr140-mhd-l7n128-accel"
+file_path = "/mnt/massive-stars/data/thomas_simulations/wr140-sims/accel_wind/wr140-mhd-l7n256-accel"
+file_basename = "wr140_mhd_accel_d3l7n256"
+quantity = "Density"
+levels = 7
 
 # List to save full names of all databases
 database_list = []
 
 # Opening database for each level
-for i in range(7):
+for i in range(levels):
     database = f"{hostname}:{os.path.join(file_path, file_basename)}_level0{i}_0000.*.silo database"
     database_list.append(database)
     OpenDatabase(database, 0)
