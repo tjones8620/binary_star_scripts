d7bb8a494c42cc14b4dc1cc5b8494ada91e86dfc
tjones8620 <thomasjones2700@gmail.com>
2023-01-26 23:14:26 +0000

    changed layout of directory

---
+++ scripts/xray-luminosity/unabsfig.py
@@ -0,0 +1,108 @@
+from matplotlib.pyplot import *
+import pandas as pd
+import astropy.units as u
+from astropy.time import Time
+import astropy.constants as c
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+plt.style.use('science')
+
+Period = 2895.00*u.day
+To = 60636.23
+epoch = Time(To-5*Period.value, format='mjd')
+eccentricity = 0.8993
+omega=227.44
+m1 = 10.31 
+m2 = 29.27
+mtot=(m1+m2)*u.Msun 
+sma = (((c.G*mtot.to(u.kg)/(4*np.pi**2)*(Period)**2).decompose())**(1./3)).to(u.AU)
+tacalc = np.arange(201)*2*np.pi/200
+sepcalc = sma*(1-eccentricity**2)/(1+eccentricity*np.cos(tacalc))
+D = 1518*u.pc
+
+cwd = "/home/visitor_ap4/code/pollockobsdata"
+
+
+rxtexlnamecor = 'Fix_excel_spreadsheet_20200916_2.0_10.0_rxte.xls'
+swiftxlnamecor = 'Fix_excel_spreadsheet_20200916_2.0_10.0_swift.xls'
+nicerxlnamecor = 'Fix_excel_spreadsheet_20200916_2.0_10.0_nicer.xls'
+
+rxteunabs = pd.read_excel(os.path.join(cwd, rxtexlnamecor), index_col=0)
+swiftunabs = pd.read_excel(os.path.join(cwd,swiftxlnamecor), index_col=0)
+nicerunabs = pd.read_excel(os.path.join(cwd, nicerxlnamecor), index_col=0)
+
+fig, ax = subplots(figsize=[15,8])
+
+x1= -3500
+x2 = 4500
+xlimit = [x1, x2]
+ylimit = [0,3]
+
+ax.set_xlabel('Time in Days relative to the Periastron passage of {0}'.format('2009-01-15'),fontsize=16)
+ax.set_ylabel('Unabsorbed L$_{x}$ (10$^{34}$ $ergs$ $s^{-1}$)',fontsize=16)
+ax.set_ylim(ylimit)
+ax.set_xlim(xlimit)
+
+scl=1e11
+jdoff = (epoch+3*Period).jd
+
+i=0
+
+minexpo = 200
+lumscl=4*np.pi*D.to(u.cm)**2/1e34
+rbkg =0.5
+wtbkg = .8 # for WT mode data
+pcbkg = 0.0
+nbkg = 0.7
+
+
+ax.plot(rxteunabs.JDMID-jdoff, (rxteunabs.UnabsFlux/1.35 - rbkg/scl)*lumscl,'ko', label='RXTE')
+t_rxte = rxteunabs.JDMID-jdoff
+l_rxte = (rxteunabs.UnabsFlux/1.35 - rbkg/scl)*lumscl
+
+pc = (swiftunabs['mode'] == 'pc')
+ax.plot(swiftunabs[pc].JDMID-jdoff, swiftunabs[pc].UnabsFlux*lumscl,'bo',label='Swift PC')
+t_swiftpc = swiftunabs[pc].JDMID-jdoff
+l_swiftpc = swiftunabs[pc].UnabsFlux*lumscl
+
+pc = (swiftunabs['mode'] == 'wt')
+ax.plot(swiftunabs[pc].JDMID-jdoff, (swiftunabs[pc].UnabsFlux-wtbkg/scl)*lumscl,'bo',label='Swift WT', markerfacecolor='none')
+t_swiftwt = swiftunabs[pc].JDMID-jdoff
+l_swiftwt = (swiftunabs[pc].UnabsFlux-wtbkg/scl)*lumscl
+
+ax.plot(nicerunabs.JDMID-jdoff, (nicerunabs.UnabsFlux - nbkg/scl)*lumscl,'go', label='NICER')
+t_nicer = nicerunabs.JDMID-jdoff
+l_nicer = (nicerunabs.UnabsFlux - nbkg/scl)*lumscl
+
+# Create a dataframe with all the data
+# df = pd.DataFrame({'t_rxte':t_rxte, 'l_rxte':l_rxte, 't_swiftpc':t_swiftpc, 'l_swiftpc':l_swiftpc, 't_swiftwt':t_swiftwt, 'l_swiftwt':l_swiftwt, 't_nicer':t_nicer, 'l_nicer':l_nicer})
+# print(df.head(50))
+# df.to_csv('wr140unabslum.csv', index=False)
+
+
+# sepnorm1=7
+
+# ax.plot(np.asarray(pcalc)*Period.value, 1./sepcalc*sepnorm1, color='grey', linestyle='--', label='1/D')
+# ax.plot(np.asarray(pcalc)*Period.value-Period.value, 1./sepcalc*sepnorm1,  color='grey', linestyle='--')
+# ax.plot(np.asarray(pcalc)*Period.value+Period.value, 1./sepcalc*sepnorm1,  color='grey', linestyle='--')
+# ax.plot(np.asarray(pcalc)*Period.value-2*Period.value, 1./sepcalc*sepnorm1,  color='grey', linestyle='--')
+
+legend(loc='upper left', ncol = 2, fontsize=12, numpoints=1, frameon=True, facecolor='white', framealpha=1)
+
+# Add phase axis on Top
+topax = ax.twiny()
+p1 = x1/Period.value
+p2 = x2/Period.value
+topax.set_xlim([p1, p2])
+topax.set_xlabel('Phase', fontsize=20)
+topax.tick_params(axis='x', labelsize=16 )
+plot()
+
+plotdir = "./images/"
+
+save=True
+if save:
+    figname=os.path.join(plotdir,'unabsorbed_luminosity.png')
+    print(f'Writing {figname}')
+    savefig(figname)
---
+++ scripts/xray-luminosity/wr140-hardxray-with-obs.py
@@ -0,0 +1,164 @@
+from luminosity import XrayLum
+import matplotlib.pyplot as plt
+import os
+from palettable.mycarta import LinearL_5
+import pathlib
+plt.style.use('science')
+home = str(pathlib.Path.home())
+import pandas as pd
+
+def main():
+    image_folder = os.path.join(home, "code/project/scripts/images/xray-lum")
+    fig, axes = plt.subplots(figsize=(2.5, 3.5))
+#     axes.set_prop_cycle('color', LinearL_5.mpl_colors)
+
+    mhd_1 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-mhd-n256.txt"), 
+            "start_time":-2.671e7,
+            "label": "mhd-1",
+            "color": "magenta",
+            "dataxmin":-150,
+            "dataxmax":135,
+            "lw": 1.5,
+            "ls": "-"
+            }
+    hd_1 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-hydro-n256.txt"), 
+            "start_time":-7.25e6,
+            "label": "hd-1",
+            "color": "C0",
+            "dataxmin":-75,
+            "dataxmax":120,
+            "lw": 1,
+            "ls": "-."
+            }
+    hd_2 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-hydro-accel-n256.txt"), 
+            "start_time":-7.24e6,
+            "label": "hd-2",
+            "color": "C6",
+            "dataxmin":-50,
+            "dataxmax":100,
+            "lw": 1.5,
+                "ls": "-"
+            }
+
+    mhd_2 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-mhd-compton-n256.txt"), 
+            "start_time":-1.25e7,
+            "label": "mhd-2",
+            "color": "C1",
+            "dataxmin":-135,
+            "dataxmax":140,
+            "lw": 1.5,
+                "ls": "-"
+            }
+
+    sim_list = [mhd_1, hd_1, hd_2, mhd_2]
+
+    for sim in sim_list:
+        plot = XrayLum(path=sim['path'], start_time=sim['start_time'], image_folder=image_folder, name=sim['label'])
+        plot.plot_band(xmin=-130,xmax=130, ymax = 5,dataxmin=sim['dataxmin'], dataxmax=sim['dataxmax'], band="hard", ax=axes, label=sim['label'], color=sim['color'], lw=sim['lw'], ls=sim['ls'])
+        # axes.legend()
+    axes.legend(loc="upper right", fontsize=8)
+
+    axes.set_ylim(0, None)
+
+    # Make sure scattered points are in front of the line
+    # axes.scatter([-100, -95, -90], [1, 1.02, 1.04], color='k', label='HD-1', zorder=10, s=3)
+    # df_path = os.path.join(home, "code/project/scripts/xray-luminosity/wr140-phasefolded-unabslum.csv")
+    # df = pd.read_csv(df_path)
+    # t_rxte = df['t_rxte'].values
+    # lum_rxte = df['l_rxte'].values
+    # t_nicer = df['t_nicer'].values
+    # lum_nicer = df['l_nicer'].values
+    # t_swiftpc = df['t_swiftpc'].values
+    # lum_swiftpc = df['l_swiftpc'].values
+    # t_swiftwt = df['t_swiftwt'].values
+    # lum_swiftwt = df['l_swiftwt'].values
+
+    
+    # axes.plot(t_rxte, lum_rxte, 'ko', label='RXTE', markersize=3)
+    # axes.plot(t_nicer, lum_nicer, 'go', label='NICER', markersize=3)
+    # axes.plot(t_swiftpc, lum_swiftpc, 'bo',label='Swift PC',markersize=3)
+    # axes.plot(t_swiftwt, lum_swiftwt, 'bo',label='Swift WT', markerfacecolor='none', markersize=3)
+    # axes.legend(loc="upper left")
+
+
+
+    fig.savefig(os.path.join(image_folder, f'xray-lum-combined-hard-narrow.png'), bbox_inches='tight', dpi=300)
+
+
+def main():
+    image_folder = os.path.join(home, "code/project/scripts/images/xray-lum")
+    fig, axes = plt.subplots(figsize=(5, 3.5))
+#     axes.set_prop_cycle('color', LinearL_5.mpl_colors)
+
+
+    # mhd_2_n128 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-compton-n128-new-updated.txt"), 
+    #         "start_time":-1.25e7,
+    #         "label": "n128",
+    #         "color": None,
+    #         "dataxmin":-130,
+    #         "dataxmax":120,
+    #         "lw": 1.5,
+    #         "ls": "-"
+    #         }
+    mhd_1 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-mhd-n256.txt"), 
+            "start_time":-2.671e7,
+            "label": "mhd-1 (No IC)",
+            "color": "magenta",
+            "dataxmin":-150,
+            "dataxmax":150,
+            "lw": 1.5,
+            "ls": "-"
+            }
+
+    mhd_2 = {"path":os.path.join(home, "code/project/scripts/xray-luminosity/xray-tables/xray-lum-wr140-mhd-compton-n256.txt"), 
+            "start_time":-1.25e7,
+            "label": "mhd-2 (With IC)",
+            "color": None,
+            "dataxmin":-130,
+            "dataxmax":160,
+            "lw": 1.5,
+            "ls": "-"
+            }
+
+    sim_list = [mhd_1, mhd_2]
+
+    for sim in sim_list:
+        plot = XrayLum(path=sim['path'], start_time=sim['start_time'], image_folder=image_folder, name=sim['label'])
+        plot.plot_band(xmin=-150, xmax=160, dataxmin=sim['dataxmin'], dataxmax=sim['dataxmax'], band="hard", ax=axes, label=sim['label'], color=sim['color'], lw=sim['lw'], ls=sim['ls'])
+        # axes.legend()
+
+    axes.legend()
+    axes.set_ylim(0, None)
+
+    df_path = os.path.join(home, "code/project/scripts/xray-luminosity/wr140-phasefolded-unabslum.csv")
+    df = pd.read_csv(df_path)
+    t_rxte = df['t_rxte'].values
+    lum_rxte = df['l_rxte'].values
+    t_nicer = df['t_nicer'].values
+    lum_nicer = df['l_nicer'].values
+    t_swiftpc = df['t_swiftpc'].values
+    lum_swiftpc = df['l_swiftpc'].values
+    t_swiftwt = df['t_swiftwt'].values
+    lum_swiftwt = df['l_swiftwt'].values
+
+    
+    axes.plot(t_rxte, lum_rxte, 'ko', label='RXTE ($2-10$ keV)', markersize=1.5)
+    # axes.plot(t_nicer, lum_nicer, 'go', label='NICER', markersize=3)
+    # axes.plot(t_swiftpc, lum_swiftpc, 'bo',label='Swift PC',markersize=1)
+    # axes.plot(t_swiftwt, lum_swiftwt, 'bo',label='Swift WT', markerfacecolor='none', markersize=3)
+    axes.legend(loc="upper left", fontsize=8)
+    
+    # Make facecolor white with background transparent
+    fig.patch.set_facecolor('white')
+    fig.patch.set_alpha(0.0)
+
+    # fig.savefig(os.path.join(image_folder, f'xray-mhd2-withobslum.png'), bbox_inches='tight', dpi=300)
+    fig.savefig(os.path.join(image_folder, f'xray-mhd2-withobslum-poster.png'), bbox_inches='tight', dpi=900)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
